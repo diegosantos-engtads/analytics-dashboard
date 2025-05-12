71e67d64b9b09562c4d4569ddb941d74042c9f31
Diego Santos <diegosantos.engtads@gmail.com>
2025-05-12 20:36:43 -0300

    feat(invoices): create form invoices, and table invoices

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -13,7 +13,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
       <form action={createInvoice}>
           <div className="rounded-md bg-gray-50 p-4 md:p-6">
-              {/* Customer Name */}
+
               <div className="mb-4">
                   <label
                       htmlFor="customer"
